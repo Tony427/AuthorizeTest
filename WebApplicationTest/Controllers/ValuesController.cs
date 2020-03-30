f30a95552a600526968b82a81ccc82838ddea376
TonyTsai <tony@unifiedpractice.com>
2020-03-30 15:39:43 +0800

    demo code

---
+++ WebApplicationTest/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace WebApplicationTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [Authorize(Policy = "IsTony")]
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var claim = (ClaimsIdentity)HttpContext.User.Identity;
+            var name = claim.FindFirst(c => c.Type == ClaimTypes.Name).Value;
+
+            return new string[] { "value1", "value2", name };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplicationTest/Middleware/TestMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace WebApplicationTest.Middleware
+{
+    internal class TestMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public TestMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            var claimsIdentity = (ClaimsIdentity)context.User.Identity;
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, "IamNotTony"));
+            //claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, "Tony"));
+            context.User = new ClaimsPrincipal(claimsIdentity);
+
+            return next(context);
+        }
+    }
+
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseTestMiddleware(this IApplicationBuilder builder)
+        {
+            builder.UseMiddleware<TestMiddleware>();
+        }
+    }
+}
